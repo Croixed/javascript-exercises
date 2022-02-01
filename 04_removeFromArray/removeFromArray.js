21fc2bcc05b55966430f5fd92174c9aebdb08f7a
tommy <bt@g>
2022-02-01 07:51:48 -0800

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+/*const removeFromArray = function(startingArray, removals) { //...removal will return an array of all args passed in, no matter how many
+  const indexToSplice = startingArray.indexOf(removal);
+  startingArray.splice(indexToSplice, 1);
+  return startingArray
+}; */
 
+const removeFromArray = function(startingArray, ...removals) { 
+  for (const removal of removals) {
+    if (startingArray.includes(removal)) {
+      let indexToSplice = startingArray.indexOf(removal);
+      startingArray.splice(indexToSplice, 1);
+    }
+  }
+  return startingArray
 };
 
 // Do not edit below this line
