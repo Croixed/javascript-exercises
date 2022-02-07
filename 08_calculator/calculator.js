1c7d22be1548c4147838b3ea2cec5fead9c575a4
tommy <bt@g>
2022-02-07 09:06:33 -0800

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let sum = a - b;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let returnVal = 0;
+  for (let i in arr) {
+    returnVal += arr[i];
+  };
+  return returnVal;
 };
 
-const multiply = function() {
-
+const multiply = function(inputs) {
+  let returnVal = 1;
+  for (let i in inputs) {
+    returnVal *= inputs[i];
+  };
+  return returnVal;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	return num **= exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+	let returnVal = 1;
+  for (let i = factor; i > 0; i--) {
+    //technically multiplies in reverse order, but it shouldn't matter
+    returnVal *= i;
+  }
+  return returnVal;
 };
 
 // Do not edit below this line
