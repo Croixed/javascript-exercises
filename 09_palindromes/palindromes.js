4c2c3c0461f40aad24002a2033faecfb27796092
tommy <bt@g>
2022-02-07 09:32:24 -0800

    completed palindrome with neat regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+  //regex to filter out all non alphanumeric in input
+  //then split, reverse, and rejoin the array and assign it to rev
+  inputString = inputString.replace(/\W/g, '').toLowerCase();
+  revString = inputString.split('').reverse().join('');
+  if (inputString === revString) {
+    return true
+  } else {
+    return false
+  };
 };
 
 // Do not edit below this line
